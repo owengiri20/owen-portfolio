10a294abee9e038a478a3ec1207de7f3b0942c32
owengiri20 <owengiri20@gmail.com>
2022-12-17 23:55:09 +0800

    add more structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 import OwenJPG from "./assets/owen.jpg";
@@ -7,6 +8,8 @@ function App() {
     <div className="app-wrapper">
       <Hero />
       <AboutMe />
+      <WorkExperience />
+      <SideProjects />
     </div>
   );
 }
@@ -36,33 +39,125 @@ const AboutMe = () => {
     "React",
     "Golang",
     "SQL",
-    "GIT",
+    "Git",
     "Github",
   ];
+
   return (
     <div className="about-me">
       <div className="about-me-intro">
-        <h2>ABOUT ME</h2>
+        <h2>ABOUT ME 🏃 🏋️ 🧋 🥋 ☕ </h2>
       </div>
 
       <div className="about-me-columns">
         <div>
           <h3>GET TO KNOW ME</h3>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-            Cupiditate, beatae animi repellendus eveniet laboriosam consectetur
-            quia totam a odit aperiam ipsa ducimus voluptate, porro ut quidem
-            magni omnis aspernatur placeat!
+          <p className="get-to-know-me-description">
+            Hi, my name is Owen and I am a full stack software developer. In my
+            professional life, I am skilled in a variety of programming
+            languages and tools such as Go, JavaScript, React, SQL, and Git. As
+            a developer, I am always looking to learn and expand my skillset. In
+            my personal life, I am passionate about staying active and healthy.
+            Some of my hobbies include martial arts, running, and overall
+            fitness. I also enjoy drawing and creating art in my free time.
+            Overall, I am a well-rounded individual with a love for both my
+            profession and personal interests. I am always looking for new
+            opportunities to grow and learn, both personally and professionally.
           </p>
+          <p>- ChatGPT (:</p>
         </div>
         <div className="about-me-skills">
           <h3>SKILLS</h3>
           <div className="skills-container">
             {skills.map((s) => {
-              return <div className="skill-tag">{s}</div>;
+              return (
+                <div key={s} className="skill-tag">
+                  {s}
+                </div>
+              );
             })}
           </div>
         </div>
+      </div>
+    </div>
+  );
+};
+
+const WorkExperience = () => {
+  const skills = ["HTML", "CSS", "Javascript", "React"];
+  return (
+    <div className="work-experience">
+      <div className="about-me-intro">
+        <h2>WORK EXPERIENCE</h2>
+        <p>Ninja Software / Syndicate 2019 - 2022</p>
+      </div>
+
+      <div className="skills-container">
+        {skills.map((s) => {
+          return <ExperienceCard key={s} />;
+        })}
+      </div>
+    </div>
+  );
+};
+
+const ExperienceCard = () => {
+  return (
+    <div className="experience-card">
+      <div className="time-span">2020-2021</div>
+
+      <div className="summary">
+        <h3>project name</h3>
+        <p>
+          projec desc Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+          Officiis exercitationem nostrum soluta aperiam animi sit ut minima,
+          ratione numquam reiciendis veniam ipsa quod expedita maiores rem
+          impedit possimus quaerat porro.
+        </p>
+        <div>Golang | Typescript | React | Material UI</div>
+      </div>
+    </div>
+  );
+};
+
+const SideProjects = () => {
+  const skills = ["1", "2", "3", "4"];
+  return (
+    <div className="work-experience">
+      <div className="about-me-intro">
+        <h2>SIDE PROJECTS</h2>
+        <p>Projects I've done in my spare time</p>
+      </div>
+
+      <div className="skills-container">
+        {skills.map((s) => {
+          return <ProjectCard key={s} />;
+        })}
+      </div>
+    </div>
+  );
+};
+
+const ProjectCard = () => {
+  return (
+    <div className="project-card">
+      {/* image */}
+      <div
+        className="project-image"
+        style={{
+          backgroundImage: `url(${OwenJPG})`,
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+        }}
+      ></div>
+      <div className="project-summary">
+        <h3>Project Name</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus
+          quos, iure exercitationem facere asperiores veritatis error, ipsum
+          minima, consectetur obcaecati qui! Numquam quasi nihil ipsa
+          consectetur aut quae mollitia quod!
+        </p>
       </div>
     </div>
   );
