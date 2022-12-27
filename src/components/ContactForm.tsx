e3f4849a9a8bb4532fa187f6539bbbe451400d88
owengiri20 <owengiri20@gmail.com>
2022-12-28 00:35:35 +0800

    get proper inputs

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,4 +1,11 @@
+import { useState } from "react";
+
 export const ContactForm = () => {
+  const [input, setInput] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
   function encode(data: any) {
     return Object.keys(data)
       .map(
@@ -8,12 +15,15 @@ export const ContactForm = () => {
   }
 
   const handleSubmit = (event: any) => {
+    console.log(input);
+
     event.preventDefault();
     fetch("/", {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
       body: encode({
         "form-name": event.target.getAttribute("name"),
+        ...input,
       }),
     }).catch((error) => alert(error));
   };
@@ -24,17 +34,36 @@ export const ContactForm = () => {
         <input type="hidden" name="form-name" value="contact" />
         <p>
           <label>
-            Your Name: <input type="text" name="name" />
+            Name:{" "}
+            <input
+              onChange={(e) => {
+                setInput((p) => ({ ...p, name: e.target.value }));
+              }}
+              type="text"
+              name="name"
+            />
           </label>
         </p>
         <p>
           <label>
-            Your Email: <input type="email" name="email" />
+            Email:{" "}
+            <input
+              onChange={(e) => {
+                setInput((p) => ({ ...p, email: e.target.value }));
+              }}
+              name="email"
+            />
           </label>
         </p>
         <p>
           <label>
-            Message: <textarea name="message"></textarea>
+            Message:{" "}
+            <textarea
+              onChange={(e) => {
+                setInput((p) => ({ ...p, message: e.target.value }));
+              }}
+              name="message"
+            ></textarea>
           </label>
         </p>
         <p>
