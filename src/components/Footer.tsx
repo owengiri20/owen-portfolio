55e59a09cc90a18f7a134ec62279ade86b581c42
owengiri20 <owengiri20@gmail.com>
2022-12-28 14:19:53 +0800

    add styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,11 @@
+import { useNavigate } from "react-router";
+
 export const Footer = () => {
+  const nav = useNavigate();
   return (
     <footer id="contact">
       <div className="socials">
+        <a onClick={() => nav("/contact")}>Contact Me</a>
         <a href="https://www.linkedin.com/in/owen-giri-0635b3178/">LinkedIn</a>
         <a href="https://github.com/owengiri20">Github</a>
         <a href="mailto:owengiri20@gmail.com">Email</a>
